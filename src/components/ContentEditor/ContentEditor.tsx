eee4c2d0b3630a69bcf8e24d6712f37443925eaf
Micah Galizia <micahgalizia@gmail.com>
2023-01-31 17:52:52 -0500

    ok, basic controls in place

--- src/components/ContentEditor/ContentEditor.tsx
+++ src/components/ContentEditor/ContentEditor.tsx
@@ -1,12 +1,47 @@
-import React, { FC } from 'react';
+import { createRef, useEffect } from 'react';
+import { IMG_URI, loadImage, renderImage } from '../../utils/drawing';
 import styles from './ContentEditor.module.css';
 
 interface ContentEditorProps {}
 
-const ContentEditor: FC<ContentEditorProps> = () => (
-  <div className={styles.ContentEditor} data-testid="ContentEditor">
-    ContentEditor Component
-  </div>
-);
+const ContentEditor = () => {
+
+  const contentCanvasRef = createRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    const canvas = contentCanvasRef.current;
+    if (!canvas) {
+      // TODO SIGNAL ERROR
+      console.error(`Unable to get canvas ref`);
+      return;
+    }
+
+    const ctx = canvas.getContext('2d', { alpha: false });
+    if (!ctx) {
+      // TODO SIGNAL ERROR
+      console.error(`Unable to get canvas context`);
+      return;
+    }
+
+    loadImage(IMG_URI)
+      .then(img => renderImage(img, canvas, ctx))
+      .catch(err => {
+        // TODO SIGNAL ERROR
+        console.log(`Unable to load image: ${JSON.stringify(err)}`);
+      });
+  });
+  return (
+    <div className={styles.ContentEditor} data-testid="ContentEditor">
+
+      <div className={styles.ContentContainer} data-testid="RemoteDisplayComponent">
+        <canvas id='fow' className={styles.ContentCanvas} ref={contentCanvasRef}>Sorry, your browser does not support canvas.</canvas>
+      </div>
+      <div className={styles.ControlsContainer}>
+        <button>Pan and Zoom</button>
+        <button>Obscure</button>
+      </div>
+    </div>
+  );
+}
 
 export default ContentEditor;
---
+++ src/utils/drawing.ts
@@ -0,0 +1,54 @@
+import { calculateBounds } from "./geometry";
+
+export const IMG_URI: string = 'map-gnomegarde-pc.jpg';
+export const CONTROLS_HEIGHT = 46;
+
+/**
+ * Load an image.
+ * @param uri the URI of the image to load
+ * @returns a promise that resolves to an HTMLImageElement
+ */
+export function loadImage(uri: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+
+    const img = new Image();
+    img.id = 'mapImage'
+    img.onload = function() { resolve(this as HTMLImageElement); }
+    img.onerror = function() { reject('Image load failed'); }
+    img.src = uri;
+  });
+}
+
+export function renderImage(image: HTMLImageElement, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+
+  const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - CONTROLS_HEIGHT;
+
+  console.log(`Image  is ${image.width} x ${image.height}`);
+  console.log(`Window is ${width} x ${height}`);
+  console.log(`Adjusted window is ${width} x ${height}`)
+
+  canvas.width = width;
+  canvas.height = height;
+  canvas.style.width = `${width}px`;
+  canvas.style.height = `${height}px`;
+
+  if (!ctx) {
+    // TODO SIGNAL ERROR
+    console.error(`Unable to get canvas context`);
+    return;
+  }
+
+  let bounds = calculateBounds(canvas.width, canvas.height, image.width, image.height);
+  console.log(`Scaled Image is ${bounds.width} x ${bounds.height}`)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);
+  if (bounds.rotate) {
+    ctx.rotate(90 * Math.PI/180);
+  }
+  ctx.drawImage(image, -bounds.width/2, -bounds.height/2, bounds.width, bounds.height);
+  ctx.restore();
+
+  ctx.save();
+}
