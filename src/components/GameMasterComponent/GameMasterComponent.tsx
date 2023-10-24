ce8a8fc4cd2a8a451c05158ed0f28aa92ccc1c09
Micah Galizia <micahgalizia@gmail.com>
2023-10-23 21:04:11 -0400

    fix lint

--- src/components/GameMasterComponent/GameMasterComponent.tsx
+++ src/components/GameMasterComponent/GameMasterComponent.tsx
@@ -3,10 +3,8 @@ import { AppBar, AppBarProps, Box, Collapse, CssBaseline, Divider, Drawer, IconB
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import PhotoIcon from '@mui/icons-material/Photo';
 import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
-import MailIcon from '@mui/icons-material/Mail';
 import LogoutIcon from '@mui/icons-material/Logout';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
