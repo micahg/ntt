85521e3b14c7c815c0f73c21307d465696c820e7
Micah Galizia <micahgalizia@gmail.com>
2023-11-07 22:47:30 -0500

    almost have detail working

--- src/components/SceneComponent/SceneComponent.tsx
+++ src/components/SceneComponent/SceneComponent.tsx
@@ -1,9 +1,10 @@
 // import styles from './SceneComponent.module.css';
 import { Box, Button, TextField, Tooltip } from '@mui/material';
-import { createRef, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { createRef, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NewSceneBundle } from '../../middleware/ContentMiddleware';
 import { Scene } from '../../reducers/ContentReducer';
+import { AppReducerState } from '../../reducers/AppReducer';
 
 const NAME_REGEX = /^[\w\s]{1,64}$/;
 
@@ -15,14 +16,19 @@ interface SceneComponentProps {
 // TODO use destructuring
 const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
   const dispatch = useDispatch();
+  
   const [player, setPlayer] = useState<File|undefined>();
   const [detail, setDetail] = useState<File|undefined>();
+  const [playerUpdated, setPlayerUpdated] = useState<boolean>(false);
+  const [detailUpdated, setDetailUpdated] = useState<boolean>(false);
+  const [resolutionMismatch, setResolutionMismatch] = useState<boolean>(false)
   const [name, setName] = useState<string>();
   const [creating, setCreating] = useState<boolean>(false);
   const [nameError, setNameError] = useState<string>();
-  const tableImageRef = createRef<HTMLImageElement>();
-  const userImageRef = createRef<HTMLImageElement>();
-  const disabledCreate = creating || (!name && !scene) || !!nameError || player === undefined;
+  const playerImageRef = createRef<HTMLImageElement>();
+  const detailImageRef = createRef<HTMLImageElement>();
+  const apiUrl = useSelector((state: AppReducerState) => state.environment.api);
+  const disabledCreate = creating || (!name && !scene) || !!nameError || player === undefined || resolutionMismatch;
 
   const handleNameChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     setName(event.target.value);
@@ -40,13 +46,17 @@ const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
     input.multiple = false;
     input.onchange = () => {
       if (!input.files) return;
-      if (layer === 'user') {
+      if (layer === 'detail') {
+        // TODO compare against other resolution
         setDetail(input.files[0]);
-        if (userImageRef.current) userImageRef.current.src = URL.createObjectURL(input.files[0]);
+        setDetailUpdated(true);
+        if (detailImageRef.current) detailImageRef.current.src = URL.createObjectURL(input.files[0]);
       }
-      else if (layer === 'table') {
+      else if (layer === 'player') {
+        // TODO comapre against other resolution
         setPlayer(input.files[0]);
-        if (tableImageRef.current) tableImageRef.current.src = URL.createObjectURL(input.files[0]);
+        setPlayerUpdated(true);
+        if (playerImageRef.current) playerImageRef.current.src = URL.createObjectURL(input.files[0]);
       }
       else console.error('Invalid layer');
     }
@@ -57,11 +67,8 @@ const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
     setCreating(true);
     if (scene) {
       // TODO clear overlay
-      dispatch({type: 'content/background', payload: player})
-
-      // TODO send detail -- when you get back to it, maybe its best to keep
-      // all image assets a bundle -- payload could be a dict with background
-      // overlay and detail, and we can easily wipe overlay here without actually
+      if (player && playerUpdated) dispatch({type: 'content/player', payload: player})
+      if (detail && detailUpdated) dispatch({type: 'content/detail', payload: detail})
       if (editScene) editScene();
       return;
     }
@@ -70,6 +77,23 @@ const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
     const data: NewSceneBundle = { description: name, player: player, detail: detail};
     dispatch({type: 'content/createscene', payload: data});
   }
+
+  const imageLoaded = () => {
+    if (playerImageRef.current && detailImageRef.current) {
+      if (playerImageRef.current.naturalWidth !== detailImageRef.current.naturalWidth ||
+          playerImageRef.current.naturalHeight !== detailImageRef.current.naturalHeight) {
+          setResolutionMismatch(true);
+      } else setResolutionMismatch(false);
+    }
+  }
+
+  useEffect(() => {
+    if (scene?.detailContent && detailImageRef?.current)
+      detailImageRef.current.src = `${apiUrl}/${scene.detailContent}`;
+
+    if (scene?.playerContent && playerImageRef?.current)
+      playerImageRef.current.src = `${apiUrl}/${scene.playerContent}`;
+  }, [apiUrl, scene, playerImageRef, detailImageRef]);
 
   return (
     <Box sx={{
@@ -101,7 +125,7 @@ const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
           minHeight: '25vh',
           width: '25vw'}}
         >
-          <Box component="img" ref={tableImageRef} sx={{width: '100%'}}/>
+          <Box component="img" ref={playerImageRef} sx={{width: '100%'}} onLoad={() => imageLoaded()}/>
         </Box>
         <Box sx={{
           display: 'flex',
@@ -113,7 +137,7 @@ const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
           minHeight: '25vh',
           width: '25vw'}}
         >
-          <Box component="img" ref={userImageRef} sx={{width: '100%'}}/>
+          <Box component="img" ref={detailImageRef} sx={{width: '100%'}} onLoad={() => imageLoaded()}/>
         </Box>
       </Box>
       <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '1em'}}>
@@ -121,7 +145,7 @@ const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
           <span>
             <Button
               variant="outlined"
-              onClick={() => selectFile('table')}
+              onClick={() => selectFile('player')}
             >
               Player Background
             </Button>
@@ -131,7 +155,7 @@ const SceneComponent = ({scene, editScene}: SceneComponentProps) => {
           <span>
             <Button
               variant="outlined"
-              onClick={() => selectFile('user')}
+              onClick={() => selectFile('detail')}
             >
               Detailed Background
             </Button>
