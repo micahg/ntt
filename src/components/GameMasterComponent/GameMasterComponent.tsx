9d57a595880d55feb7094c1f88f5076496007696
Micah Galizia <micahgalizia@gmail.com>
2024-01-03 12:41:04 -0500

    fix: put padding back in main component

--- src/components/GameMasterComponent/GameMasterComponent.tsx
+++ src/components/GameMasterComponent/GameMasterComponent.tsx
@@ -50,11 +50,7 @@ const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
   open?: boolean;
 }>(({ theme, open }) => ({
   flexGrow: 1,
-  // padding: theme.spacing(3),
-  // width: `calc(100% - ${drawerWidth}px)`,
-  // height: `calc(100% - ${appBarHeight})`,
-  // top: appBarHeight,
-  padding: theme.spacing(0),
+  padding: theme.spacing(1),
   transition: theme.transitions.create("margin", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
